5389dec56f132eceb152e391fdf45f605119f70e
GeoProgrammer99 <geoffreywislez@gmail.com>
2020-01-17 15:04:29 +0100

    test php

---
+++ entrainement.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<link rel ="stylesheet" href="style.css"/>
+<meta charset = "UTF-8"/>
+<title>PageTest</title>
+</head>
+<body>
+<section>
+    <article></article>
+
+</section>
+</body>
+
+
+
+</html>
